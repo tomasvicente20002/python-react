16161ea3229278f8be724769fe6db18da6ee4cd5
Tomás vicente <56982030+tomasvicente20002@users.noreply.github.com>
2025-06-16 11:53:45 +0100

    Remove debug print from password hashing

--- backend/models.py
+++ backend/models.py
@@ -13,7 +13,6 @@ class User(db.Model):
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-        print(f"Lenght {len(self.password_hash)}")
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
